2d00f297aef85bfb2936ae615c034bcc3ee3994c
Bryan Parker <bryan.parker24@gmail.com>
2026-04-22 08:38:15 -0500

    small padding tweak to vizzes card wrapper

--- apps/hub/src/lib/embed-template.ts
+++ apps/hub/src/lib/embed-template.ts
@@ -46,7 +46,7 @@ export function renderEmbed({
         #wrap {
             width: 100%;
             height: 100%;
-            padding: 16px;
+            padding: 10px;
             display: flex;
             align-items: center;
             justify-content: center;
